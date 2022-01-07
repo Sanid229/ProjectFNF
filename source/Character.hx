4144563e8526f0318c69e3f005895a86b3db6e10
Sanid229 <96082231+Sanid229@users.noreply.github.com>
2022-01-07 21:57:25 +0300

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -499,14 +499,21 @@ class Character extends FlxSprite
 				addOffset("singDOWN-alt", -30, -27);
 
 				playAnim('idle');
-		         case 'seagush':
-				tex = Paths.getSparrowAtlas('characters/Seagush');
+		        case 'fred':
+				// DAD ANIMATION LOADING CODE
+				tex = Paths.getSparrowAtlas('Fred');
 				frames = tex;
-				animation.addByPrefix('idle', "idle", 24, false);
-				animation.addByPrefix('singUP', 'up', 24, false);
-				animation.addByPrefix('singDOWN', 'down', 24, false);
-				animation.addByPrefix('singLEFT', 'left', 24, false);
-				animation.addByPrefix('singRIGHT', 'right', 24, false);
+				animation.addByPrefix('idle', 'Dad idle dance', 24);
+				animation.addByPrefix('singUP', 'Dad Sing Note UP', 24);
+				animation.addByPrefix('singRIGHT', 'Dad Sing Note RIGHT', 24);
+				animation.addByPrefix('singDOWN', 'Dad Sing Note DOWN', 24);
+				animation.addByPrefix('singLEFT', 'Dad Sing Note LEFT', 24);
+
+				addOffset('idle');
+				addOffset("singUP", -6, 50);
+				addOffset("singRIGHT", 0, 27);
+				addOffset("singLEFT", -10, 10);
+				addOffset("singDOWN", 0, -30);
 
 				playAnim('idle');
 		}
